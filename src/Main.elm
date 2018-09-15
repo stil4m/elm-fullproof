e36e81fa974f3ff2af490083ee4504221f57b14b
Mats Stijlaart <mstijlaart@gmail.com>
2018-09-15 14:24:41 +0200

    Setup TEA

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,25 @@
 module Main exposing (main)
 
+import Browser
 import Html
 
 
 main =
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
+
+
+init =
+    { people = [] }
+
+
+update msg model =
+    model
+
+
+view model =
     Html.div []
         [ Html.text "Hello world!" ]
